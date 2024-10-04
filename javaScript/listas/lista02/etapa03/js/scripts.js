ba26269e4e8f0f40a275ab8ef787c6f95e276585
Xande <Alexandre.froes@estudante.iftm.edu.br>
2024-10-04 11:28:24 -0300

    resolução da lista02 de exercícios de javascript básico

---
+++ javaScript/listas/lista02/etapa03/js/scripts.js
@@ -0,0 +1,17 @@
+var nomeDisciplinas = ["Projeto Sistema Web MVC e SQL","Algoritmos e Programação","Programação Orientada a Objetos 1","Fundamentos de Banco de Dados","JavaScript Básico"];
+
+var abreviacaoDisciplinas = ["P2","AP","POO1","FBD","JSB"];
+
+document.write(`<table>`);
+document.write(`    <tr>`)
+document.write(`        <th>Código</th>`)
+document.write(`        <th>Disciplina</th>`)
+document.write(`    </tr>`)
+
+for (var i in abreviacaoDisciplinas) {
+    document.write(`    <tr>`)
+    document.write(`        <td>${abreviacaoDisciplinas[i]}</td>`)
+    document.write(`        <td>${nomeDisciplinas[i]}</td>`)
+    document.write(`    </tr>`)
+}
+document.write(`</table>`);
