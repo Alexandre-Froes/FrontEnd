9aec0e1907df9b3b5b01d7d4c149c61755d0fd73
Xande <Alexandre.froes@estudante.iftm.edu.br>
2025-02-11 12:44:01 -0300

    Envio definitivo da lista 10 e 11

--- javaScript/listas/lista10&11/lista11/js/cadastro.js
+++ javaScript/listas/lista10&11/lista11/js/cadastro.js
@@ -4,25 +4,40 @@ document.addEventListener("DOMContentLoaded", function() {
     const senha = document.getElementById("txtSenha");
     const senhaValidacao = document.getElementById("txtSenhaValidacao");
 
+    const somErro = new Audio("audio/erro.mp3");
+    const somSucesso = new Audio("audio/sucesso.mp3");
+
+    var usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
+
     entrar.addEventListener("click", function(){
-        for(login in usuarios) {
-            if(login == usuarios.login) {
-                alert("Usuário já cadastrado");
-                return;
+        var usuarioExistente = false;
+        for (let i = 0; i < usuarios.length; i++) {
+            if (usuarios[i].login === login.value) {
+                usuarioExistente = true;
+                break;
             }
         }
-        
-        if(login.value == "" || senha.value == "" || senhaValidacao.value == "") {
-            alert("Nome ou senha não preenchidos");
+        if (usuarioExistente) {
+            msgUser("erro", "Usuário já cadastrado");
+        } else if(login.value == "" || senha.value == "" || senhaValidacao.value == "") {
+            msgUser("erro", "Preencha todos os campos");
 
         } else if (senha.value != senhaValidacao.value) {
-            alert("Senhas não conferem");
+            msgUser("erro", "Senhas não conferem");
 
         } else {
-            var usuarios = 
-            {"login": login.value, "senha": senha.value};
+            msgUser("sucesso", "Usuario cadastrado com sucesso!");
+
+            var novoUser = 
+            {login: login.value, senha: senha.value};
+            usuarios.push(novoUser);
             
             localStorage.setItem("usuarios", JSON.stringify(usuarios));
         }
     });
+
+    function msgUser(tipo, msg) {
+        var som = tipo == "erro" ? somErro.play() : somSucesso.play();
+        alert(msg);
+    }
 });
--- javaScript/listas/lista10&11/lista11/js/login.js
+++ javaScript/listas/lista10&11/lista11/js/login.js
@@ -3,20 +3,40 @@ document.addEventListener("DOMContentLoaded", function() {
     const login = document.getElementById("txtLogin");
     const senha = document.getElementById("txtSenha");
 
-    var jsonObj = JSON.parse(localStorage.getItem("usuarios"));
+    const somErro = new Audio("audio/erro.mp3");
+    const somSucesso = new Audio("audio/sucesso.mp3");
+
+    var usuarios = JSON.parse(localStorage.getItem("usuarios"));
 
     entrar.addEventListener("click", function(){
         if (senha.value == "" || login.value == "") {
-            alert("Usuário ou senha não preenchidos.");
+            msgUser("erro", "Preencha todos os campos.");
+            return;
             
-        } else if(login.value != jsonObj.login) {
-            alert("Usuário não cadastrado.");
+        }
 
-        } else if (jsonObj.login == login.value && jsonObj.senha == senha.value) {
+        let usuarioEncontrado = null;
+        for (let i = 0; i < usuarios.length; i++) {
+            if (usuarios[i].login === login.value) {
+                usuarioEncontrado = usuarios[i];
+                break;
+            }
+        }
+        
+        if(!usuarioEncontrado) {
+            msgUser("erro", "Usuário não encontrado.");
+
+        } else if (usuarioEncontrado.login == login.value 
+        && usuarioEncontrado.senha == senha.value) {
             window.location.href = "../lista10/index.html";
 
         } else {
-            alert("Usuário ou senha inválidos.");
+            msgUser("erro", "Usuário ou senha inválidos.");
         }
     });
+
+    function msgUser(tipo, msg) {
+        var som = tipo == "erro" ? somErro.play() : somSucesso.play();
+        alert(msg);
+    }
 });
